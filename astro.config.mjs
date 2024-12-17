cc23cafb4baa229356db5b85369accc538f975bd
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2024-12-17 23:43:28 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// TODO: Add sitemap maybe
+export default defineConfig({
+    devToolbar: {
+        enabled: true
+    }
+});
