e92f402abfb1bc35d6c986316d6600f9c4c84b18
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-01-02 15:21:50 +0200

    Set up basic tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 // TODO: Add sitemap maybe
 export default defineConfig({
-    devToolbar: {
-        enabled: true,
-    },
+  devToolbar: {
+      enabled: true,
+  },
+
+  integrations: [tailwind()],
 });
