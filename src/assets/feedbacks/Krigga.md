c93d97a0e7265534789b74ea44ccffd9341ec1df
Leons Aleksandrovs <58330666+Skrazzo@users.noreply.github.com>
2025-01-19 22:57:54 +0200

    Skills fixed, now doing feedbacks

---
+++ src/assets/feedbacks/Krigga.md
@@ -0,0 +1,7 @@
+---
+fromName: Jēkabs Krīgerts
+stars: 4
+fullFeedbackLink: sashdjhasjkdd
+---
+
+However, what makes Leon stand out most among programmers is his attitude, understanding and ability to resolve business-critical issues, such as bugs in the release or production environment, as quickly and efficiently as possible at any time of the day or night. A responsible programmer who reacts, thinks and does.
---
+++ src/content.config.ts
@@ -0,0 +1,25 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from "astro:content";
+
+// 2. Import loader(s)
+import { glob } from "astro/loaders";
+
+// 3. Create collections
+const feedbacks = defineCollection({
+    loader: glob({ pattern: "**/*.md", base: "./src/assets/feedbacks" }),
+    schema: z.object({
+        fromName: z.string(),
+        stars: z.number(),
+        fullFeedbackLink: z.string(),
+
+        // type: z.enum(["Space Probe", "Mars Rover", "Comet Lander"]),
+        // launch_date: z.date(),
+        // status: z.enum(["Active", "Inactive", "Decommissioned"]),
+        // destination: z.string(),
+        // operator: z.string(),
+        // notable_discoveries: z.array(z.string()),
+    }),
+});
+
+// 4. export collections
+export const collections = { feedbacks };
